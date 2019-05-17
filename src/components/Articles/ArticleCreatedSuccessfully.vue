3dbaf47e4199931b5761b633d57ea20ac67e0b2e
Bastien <bastien.proust@univ.uphf.fr>
2019-05-17 16:51:43 +0200

    front end page ArticleCreatedSuccessfully

--- src/components/Articles/ArticleCreatedSuccessfully.vue
+++ src/components/Articles/ArticleCreatedSuccessfully.vue
@@ -1,3 +1,21 @@
 <template>
-    
+  <!--page confirmation de création d'un article-->
+  <v-layout justify-space-around pa-5>
+    <v-flex md4>
+      <v-card
+        flat
+        style="border:5px solid #01D758;border-radius:20px;-moz-border-radius:20px;-webkit-border-radius:20px;"
+      >
+        <v-img
+          src="https://firebasestorage.googleapis.com/v0/b/gpufinal.appspot.com/o/ok-2282499_960_720.gif?alt=media&token=dd707bdd-1a20-4e69-87e7-0f560132d111"
+        ></v-img>
+        <v-card-text style="font-size:30px;color:#009D9D">
+          <p> L'article que vous avez proposé sera analysé et approuvé par notre équipe dans les plus brefs délais.</p>
+          <p>
+            <br> Merci pour votre participation et votre soutien à notre projet.
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
--- src/router.js
+++ src/router.js
@@ -19,185 +19,177 @@ import XboxGames from './components/Games/XboxGames'
 import SwitchGames from './components/Games/SwitchGames'
 import PcGames from './components/Games/PcGames'
 import Contact from './components/Games/SearchedGame'
+import ArticleCreated from './components/Articles/ArticleCreatedSuccessfully'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-      meta:
-      {
-        title: 'Accueil - Game Players Union'
-      }
-    },
-    {
-      path: '/Game',
-      name: 'Game',
-      component: Game,
-      meta:
-      {
-        title: 'Jeux - Game Players Union'
-      }
-    },
-    {
-      path: '/AllGames',
-      name: 'AllGames',
-      component: AllGames,
-      meta:
-      {
-        title: 'Tous les jeux - Game Players Union'
-      }
-    },
-    {
-      path: '/GameRank',
-      name: 'GameRank',
-      component: GameRank,
-      meta:
-      {
-        title: 'Classement - Game Players Union'
-      }
-    },
-    {
-      path: '/RecentGames',
-      name: 'RecentGames',
-      component: RecentGames,
-      meta:
-      {
-        title: 'Ajout récent - Game Players Union'
-      }
-    },
-    {
-      path: '/News',
-      name: 'News',
-      component: News,
-      meta:
-      {
-        title: 'News - Game Players Union'
-      }
-    },
-    {
-      path: '/PlaystationGames',
-      name: 'PlaystationGames',
-      component: PlaystationGames,
-      meta:
-      {
-        title: 'Jeux Playstation - Game Players Union'
-      }
-    },
-    {
-      path: '/XboxGames',
-      name: 'XboxGames',
-      component: XboxGames,
-      meta:
-      {
-        title: 'Jeux Xbox - Game Players Union'
-      }
-    },
-    {
-      path: '/SwitchGames',
-      name: 'SwitchGames',
-      component: SwitchGames,
-      meta:
-      {
-        title: 'Jeux Switch - Game Players Union'
-      }
-    },
-    {
-      path: '/PcGames',
-      name: 'PcGames',
-      component: PcGames,
-      meta:
-      {
-        title: 'Jeux PC - Game Players Union'
-      }
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: Signup,
-      meta:
-      {
-        title: 'S\'inscrire - Game Players Union'
-      }
-    },
-    {
-      path: '/signin',
-      name: 'signin',
-      component: Signin,
-      meta:
-      {
-        title: 'Se connecter - Game Players Union'
-      }
-    },
-    {
-      path: '/gamedesc',
-      name: 'GameDesc',
-      component: GameDesc,
-      meta:
-      {
-        title: 'Description - Game Players Union'
-      }
-    },
-    {
-      path: '/settings',
-      name: 'settings',
-      component: Settings,
-      meta:
-      {
-        title: 'Paramètres de Compte - Game Players Union'
-      },
-      //beforeEnter:authGuard
-    },
-    {
-      path: '/suggest_game',
-      name : 'suggestgame',
-      component : GameSuggestion,
-      meta:
-      {
-        title: "Suggestion d'un jeux " 
-      },
-      //beforeEnter:authGuard
-    },
-    {
-      path: '/suggested_games',
-      name : 'suggestedgames',
-      component : GameSuggestions,
-      meta:
-      {
-        title: "Jeux suggérés " 
-      },
-      //beforeEnter:authGuard
-    },
-    {
-      path: '/game_created',
-      name : 'gameCreated',
-      component : GameCreated,
-      meta:
-      {
-        title: "Jeu suggéré avec succès" 
-      },
-      //beforeEnter:authGuard
-    },
-    {
-      path: '/about',
-      name: 'about',
-      meta:
-      {
-        title: 'A propos - Game Players Union'
-      },
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/contacts/:id', 
-      component: Contact 
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home,
+            meta: {
+                title: 'Accueil - Game Players Union'
+            }
+        },
+        {
+            path: '/Game',
+            name: 'Game',
+            component: Game,
+            meta: {
+                title: 'Jeux - Game Players Union'
+            }
+        },
+        {
+            path: '/AllGames',
+            name: 'AllGames',
+            component: AllGames,
+            meta: {
+                title: 'Tous les jeux - Game Players Union'
+            }
+        },
+        {
+            path: '/GameRank',
+            name: 'GameRank',
+            component: GameRank,
+            meta: {
+                title: 'Classement - Game Players Union'
+            }
+        },
+        {
+            path: '/RecentGames',
+            name: 'RecentGames',
+            component: RecentGames,
+            meta: {
+                title: 'Ajout récent - Game Players Union'
+            }
+        },
+        {
+            path: '/News',
+            name: 'News',
+            component: News,
+            meta: {
+                title: 'News - Game Players Union'
+            }
+        },
+        {
+            path: '/PlaystationGames',
+            name: 'PlaystationGames',
+            component: PlaystationGames,
+            meta: {
+                title: 'Jeux Playstation - Game Players Union'
+            }
+        },
+        {
+            path: '/XboxGames',
+            name: 'XboxGames',
+            component: XboxGames,
+            meta: {
+                title: 'Jeux Xbox - Game Players Union'
+            }
+        },
+        {
+            path: '/SwitchGames',
+            name: 'SwitchGames',
+            component: SwitchGames,
+            meta: {
+                title: 'Jeux Switch - Game Players Union'
+            }
+        },
+        {
+            path: '/PcGames',
+            name: 'PcGames',
+            component: PcGames,
+            meta: {
+                title: 'Jeux PC - Game Players Union'
+            }
+        },
+        {
+            path: '/signup',
+            name: 'signup',
+            component: Signup,
+            meta: {
+                title: 'S\'inscrire - Game Players Union'
+            }
+        },
+        {
+            path: '/signin',
+            name: 'signin',
+            component: Signin,
+            meta: {
+                title: 'Se connecter - Game Players Union'
+            }
+        },
+        {
+            path: '/gamedesc',
+            name: 'GameDesc',
+            component: GameDesc,
+            meta: {
+                title: 'Description - Game Players Union'
+            }
+        },
+        {
+            path: '/settings',
+            name: 'settings',
+            component: Settings,
+            meta: {
+                title: 'Paramètres de Compte - Game Players Union'
+            },
+            //beforeEnter:authGuard
+        },
+        {
+            path: '/suggest_game',
+            name: 'suggestgame',
+            component: GameSuggestion,
+            meta: {
+                title: "Suggestion d'un jeux "
+            },
+            //beforeEnter:authGuard
+        },
+        {
+            path: '/suggested_games',
+            name: 'suggestedgames',
+            component: GameSuggestions,
+            meta: {
+                title: "Jeux suggérés "
+            },
+            //beforeEnter:authGuard
+        },
+        {
+            path: '/game_created',
+            name: 'gameCreated',
+            component: GameCreated,
+            meta: {
+                title: "Jeu suggéré avec succès"
+            },
+            //beforeEnter:authGuard
+        },
+        {
+            path: '/ArticleCreated',
+            name: 'ArticleCreated',
+            component: ArticleCreated,
+            meta: {
+                title: "Article suggéré avec succès"
+            },
+            //beforeEnter:authGuard
+        },
+        {
+            path: '/about',
+            name: 'about',
+            meta: {
+                title: 'A propos - Game Players Union'
+            },
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () =>
+                import ( /* webpackChunkName: "about" */ './views/About.vue')
+        },
+        {
+            path: '/contacts/:id',
+            component: Contact
+        }
+    ]
 })
