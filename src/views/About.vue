4e633ff012afbcaeb7956cf6177d27784f9a3165
Younès D <younes59220@hotmail.fr>
2019-05-21 18:00:01 +0200

    Responsive About Page fixes

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,52 +2,45 @@
   <!--page a propos de GPU-->
   <v-container pa-5>
     <!--1ere ligne-->
-    <v-layout row>
-      <v-flex xs6 pa-2>
-        <v-card dark height="750" width="830">
+    <v-layout row wrap justify-center pa-1>
+      <v-flex xs12 sm10 md6 lg6 pa-1>
+        <v-card dark min-height="565">
           <v-img
-            height="300"
-            width="830"
+            max-height="250"
             src="https://firebasestorage.googleapis.com/v0/b/gpufinal.appspot.com/o/code-2620118_960_720.jpg?alt=media&token=f308359d-6c3d-4eb2-9ea8-528d731c3bf5"
           ></v-img>
           <v-card-title primary class="title">
-            <h1>
               <p style="color:#009D9D">Qui sommes-nous?</p>
-            </h1>
           </v-card-title>
           <v-card-text>
-            <p style="font-size:22px;">
+            <p style="font-size:16px;">
               Nous sommes un groupe de 4 étudiants en licence 3 de l'université Polythéchnique Haut de France.
-              <br>Nous avons créé ce site pour permettre à chacun de partager son avis et ces connaissances dans les jeux vidéo tout en apportant de la nouveauté dans ce domaine.
-              <br>
-              <br>Grâce à nous, vous pourrez en apprendre davantage sur les jeux vidéo, suivre leurs actualités et faire vos propres liste ou vous inspirer de celle des autres ou de vos amis.
             </p>
+              <p style="font-size:16px;">Nous avons créé ce site pour permettre à chacun de partager son avis et ces connaissances dans les jeux vidéo tout en apportant de la nouveauté dans ce domaine.
+              </p>
+              <p style="font-size:16px;">Grâce à nous, vous pourrez en apprendre davantage sur les jeux vidéo, suivre leurs actualités et faire vos propres liste ou vous inspirer de celle des autres ou de vos amis.
+              </p>
           </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs6 pa-2>
-        <v-card dark height="750" width="830">
+      <v-flex xs12 sm10 md6 lg6 pa-1>
+        <v-card dark min-height="565">
           <v-img
-            height="300"
-            width="830"
+          max-height="250"
             src="https://firebasestorage.googleapis.com/v0/b/gpufinal.appspot.com/o/10891456256_0b557c1564.jpg?alt=media&token=383c3ba3-33fe-44e3-a16d-ffcd361e0b0c"
           ></v-img>
           <v-card-title primary class="title">
-            <h1>
               <p style="color:#009D9D">Vous êtes acteur!</p>
-            </h1>
           </v-card-title>
           <v-card-text>
-            <p style="font-size:22px;">
+            <p style="font-size:16px;">
               En effet , notre projet se veut communautaire.
-              <br>Vos êtes important et on vous le fait comprendre en vous permettant de participer à l'amélioration de notre projet en vous permettant de donner votre avis sur le site pour l'améliorer, nous suggérer d'ajouter un jeu ou encore un article.
-              <br>
-              <br>Vous l'aurez compris, vous n'êtes pas un simple utilisateur mais un acteur à part entière de l'amélioration et de l'évolution de notre projet.
-              <br>
-              <br>
-              <br>
-              <p style="font-size:22px;" class="text-xs-right">Les fondateurs</p>
             </p>
+              <p style="font-size:16px;">Vos êtes important et on vous le fait comprendre en vous permettant de participer à l'amélioration de notre projet en vous permettant de donner votre avis sur le site pour l'améliorer, nous suggérer d'ajouter un jeu ou encore un article.
+              </p>
+              <p style="font-size:16px;">Vous l'aurez compris, vous n'êtes pas un simple utilisateur mais un acteur à part entière de l'amélioration et de l'évolution de notre projet.
+              </p>
+              <span style="font-size:16px;" class="text-xs-right">Les fondateurs</span>
           </v-card-text>
         </v-card>
       </v-flex>
